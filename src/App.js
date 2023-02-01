9355211ef71d7fd323d7f9c7fd6283f5f23a94b7
elmanehmedov <elmanehmedov66@gmail.com>
2023-02-01 12:10:36 +0400

    material-ui-repo

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import Card from "@mui/material/Card"
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+ import CssBaseLine from '@mui/material/CssBaseline'
+ import Grid from '@mui/material/CssBaseline'
+ import Toolbar from '@mui/material/Toolbar';
+ 
+
+
+import ContainedButton from './components/ContainedButton';
+import MyAppBar from './modules/MyAppBar';
+
+ 
+
+
+
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyAppBar/>
+      <ContainedButton/>
+       <Typography  mt={3} >Any Text </Typography>
+        <CssBaseLine/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContainedButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const ContainedButton = () => {
+  return (
+    
+    <Button
+     variant="contained">Hello World</Button>
+
+  )
+}
+
+export default ContainedButton
